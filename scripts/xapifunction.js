f213fc6820438d973bef874c13e3f38ddd83e271
Oliver Pincus <839393+opincus@users.noreply.github.com>
2019-03-08 15:16:07 +0100

    Update

--- scripts/xapifunction.js
+++ scripts/xapifunction.js
@@ -4,13 +4,35 @@ function CreateXAPIStatement() {
     //define the xapi statement being sent from scratch
     var statement = new ADL.XAPIStatement(
     new ADL.XAPIStatement.Agent(ADL.XAPIWrapper.hash('mailto:guest@xlearnlab.com'), 'Guest'),
-    new ADL.XAPIStatement.Verb('http://adlnet.gov/expapi/verbs/launched', 'launched'),
+    new ADL.XAPIStatement.Verb('A', 'B'),
     new ADL.XAPIStatement.Activity(document.getElementById("activityurl").value, document.getElementById("activitydescription").value)
     );
 
-    if (typeof score != "undefined") {    
-        statement.score = score;
+
+    if (typeof options.verburi != "undefined") {    
+        //  statement.verb(options.verbid, options.verb);
+        console.log('verb');
+        //statement.verb.id = ('C');
+        //statement.verb.display('en-US')= 'DDD';
+
+        statement.verb = new ADL.XAPIStatement.Verb(
+            options.verbid,
+            {
+                'en-US': options.verb
+            }
+        );
+
+    }           
+      
+    if (typeof options.objecturi != "undefined") {            
+        statement.activity = new ADL.XAPIStatement.Activity(options.objecturi, options.objectdescription)     
+    }           
+
+    //new ADL.XAPIStatement.Activity(document.getElementById("activityurl").value, document.getElementById("activitydescription").value)
+      
+    if (typeof options.score != "undefined") {    
+        statement.score = options.score;
     }
-       
+        
     return statement;    
  }
